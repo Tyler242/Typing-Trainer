360a779e50c6a34b0dd1c1e1cde07f613fffc688
Tyler242 <77693574+Tyler242@users.noreply.github.com>
2022-03-04 15:14:07 -0700

    implemented timer & wpm, removed lives

--- wpm.py
+++ wpm.py
@@ -3,45 +3,37 @@
     for tracking the users words per minute
 """
 import pygame
-import time
 # start a clock that will count the seconds from when the game starts.
 # each time a word is entered correctly, calculate the words per minute and update it
 # each time a word is entered incorrectly, calculate the words per minute with a penalty.
 
 
-class Speed:
+class WPM:
 
     def __init__(self):
         self.curr_wpm = 0
-        self.x = 900
-        self.y = 25
-
-    def start_clock(self):
-        self.start_time = time.time()
         self.correct_words = 0
-        self._compute_wpm()
+        self.x = 80
+        self.y = 436
 
-    def correct_word(self):
+    def correct_word(self, curr_time):
         # calculate and update WPM if the user entered a word correctly
-        print("correct word entered")
         self.correct_words += 1
-        self._compute_wpm()
+        self._compute_wpm(curr_time)
 
     def incorrect_word(self):
         # calculate and update WPM if the user entered a word incorrectly
         print('word not on screen')
         self._compute_wpm()
 
-    def _compute_wpm(self):
+    def _compute_wpm(self, curr_time):
         # compute the words per minute
-        elapsed_time = (time.time() - self.start_time)
-        print("Words per second: ", (elapsed_time/self.correct_words)
-              if self.correct_words > 0 else 0)
+        self.curr_wpm = round(self.correct_words / (curr_time / 60), 2)
 
     def draw_text(self, screen, text, x, y):
         # draw the WPM value on the screen
         wpm_rect = text.get_rect()
-        wpm_rect.center(x, y)
+        wpm_rect.center = (x, y)
         screen.blit(text, wpm_rect)
 
     def config_text(self, screen):
