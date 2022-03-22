e4c5d404c98500018e47fc837708d860f14ed287
Tyler242 <77693574+Tyler242@users.noreply.github.com>
2022-03-22 14:17:54 -0600

    removing commented code

--- main_screen.py
+++ main_screen.py
@@ -164,28 +164,6 @@ class MainScreen():
         text = font.render(level_str, True, BLACK)
         self.draw_text(screen, text, 80, 25)        # top left corner
 
-    # Draws the lives counter on the screen with colors
-    # def draw_lives(self, screen):
-    #     color = BLACK
-
-    #     # Draws "Lives: "
-    #     font = pygame.font.Font(None, 52)
-    #     text = font.render("Lives: ", True, color)
-    #     self.draw_text(screen, text, 80, 436)       # bottom left corner
-
-    #     # Sets color of lives based on number of lives
-    #     if self.lives >= 4:
-    #         color = GREEN
-    #     elif self.lives >= 2:
-    #         color = ORANGE
-    #     else:
-    #         color = RED
-
-    #     # Draws number of lives
-    #     text = font.render(str(self.lives), True, color)
-    #     # bottom left corner next to lives
-    #     self.draw_text(screen, text, 140, 436)
-
     # Levels up by increase speed
     def level_up(self):
         for word in self.screen_words:
